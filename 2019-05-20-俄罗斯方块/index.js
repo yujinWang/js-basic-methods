a923de1b92557aa30b9e670b13f16ba1580e8a3f
yujinWang <1219044051@qq.com>
2019-06-10 22:53:03 +0800

    地图绘制

--- 2019-05-20-俄罗斯方块/index.js
+++ 2019-05-20-俄罗斯方块/index.js
@@ -1,4 +1,18 @@
 var canvas = document.querySelector("#canvas");
 var gc = canvas.getContext("2d");
 gc.fillStyle = "red"; //填充颜色-先填充颜色，后创建方块
-gc.fillRect(100,100,200,200); //创建方块
+// gc.fillRect(100, 100, 200, 200); //创建方块
+
+var data = initMap(12, 12);
+
+//创建游戏地图方法-便于后面复用
+function initMap(row, col) {
+    var map = [];
+    for (var i = 0; i < row; i++) {
+        map.push([]);
+        for (var j = 0; j < col; j++) {
+            map[i].push(0);
+        }
+    }
+    return map;
+}
