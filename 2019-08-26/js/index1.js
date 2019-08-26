588204515bbdcf3b3d0b6261d44b5e42495a4de8
yujinWang <1219044051@qq.com>
2019-08-26 22:41:09 +0800

    2048基础

---
+++ 2019-08-26/js/index1.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+	//监听键盘按下事件
+	document.onkeydown = function(e){
+		switch (e.keyCode) {
+			//分别匹配键盘的左上右下键
+			// 从第0个值开始，将第0个的值和第一个的值进行比较
+			// 如果相等的话，将这两个值进行相加，把相加的结果赋值给第0个
+			// 同时将第一个的值设置为空
+			// i=0，j=i+1
+			// i != j+1  那么i++,继续下一位的运算
+			// i == j+1 偏移到下一位
+			case 37:
+				
+				break;
+			case 38:
+				break;
+			case 39:
+				break;
+			case 40:
+				break;
+
+		}
+	}
+}
